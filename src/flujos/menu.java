05d5b2b7a7a84fb755bc3e6d1e30cdd6c5658cec
Pilar <pilar.perez.s@estudiantes.utec.edu.uy>
2025-09-03 19:50:17 -0300

    actualizacion de menu con opciones de libro y editorial

--- src/flujos/menu.java
+++ src/flujos/menu.java
@@ -1,9 +1,14 @@
 package flujos;
 
+import java.sql.Date;
 import java.sql.SQLException;
 
+import dao.EditorialDAO;
+import dao.LibroDAO;
 import dao.autorDAO;
 import dao.generoDAO;
+import models.Libro;
+import models.Editorial;
 
 import java.util.List;
 import java.util.Scanner;
@@ -12,11 +17,14 @@ public class menu {
     private final Scanner scanner = new Scanner(System.in);
     private final autorDAO autordao;
     private final generoDAO generodao;
-
+    private final LibroDAO librodao;
+    private final EditorialDAO editorialdao;
 
     public menu() {
         this.autordao = new autorDAO();
         this.generodao = new generoDAO();
+        this.librodao = new LibroDAO();
+        this.editorialdao = new EditorialDAO();
     }
 
     public void mostrarMenu(){
@@ -33,9 +41,15 @@ public class menu {
                 System.out.println("4. Crear genero");
                 System.out.println("5. Modificar genero");
                 System.out.println("6. Eliminar genero");
-                System.out.println("9. Salir");
-
-
+                System.out.println("7. Crear libro");
+                System.out.println("8. Eliminar libro");
+                System.out.println("9. Modificar libro");
+                System.out.println("10. Listar libros");
+                System.out.println("11. Crear editorial");
+                System.out.println("12. Eliminar editorial");
+                System.out.println("13. Modificar editorial");
+                System.out.println("14. Listar editoriales");
+                System.out.println("15. Salir");
 
                 System.out.println("Opcion");
 
@@ -55,10 +69,34 @@ public class menu {
                         break;
                     case 6: eliminarGenero();
                         break;
+                    case 7:
+                        crearLibro();
+                        break;
+                    case 8:
+                        eliminarLibro();
+                        break;
                     case 9:
+                        editarLibro();
+                        break;
+                    case 10:
+                        listarLibros();
+                        break;
+                    case 11:
+                        crearEditorial();
+                        break;
+                    case 12:
+                        eliminarEditorial();
+                        break;
+                    case 13:
+                        editarEditorial();
+                        break;
+                    case 14:
+                        listarEditoriales();
+                        break;
+                    case 15:
                         break;
                 }
-            }while(opcion != 9);
+            }while(opcion != 15);
 
 
         }catch(Exception e){
@@ -135,9 +173,9 @@ public class menu {
         int id = scanner.nextInt();
 
         System.out.println("Ingrese el nuevo nombre del genero");
-        String nomvre = scanner.next();
+        String nombre = scanner.next();
 
-        generoDAO.modificarGenero(id,nomvre);
+        generoDAO.modificarGenero(id,nombre);
 
     }
 
@@ -149,7 +187,98 @@ public class menu {
 
         generoDAO.eliminarGenero(id);
     }
+    public void crearLibro() throws SQLException {
+        System.out.println("Ingrese el titulo del libro: ");
+        String titulo = scanner.nextLine();
 
+        System.out.println("Ingrese el isbn del libro: ");
+        int isbn = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Ingrese la fecha de publicacion del libro: ");
+        Date fechaPublicacion = Date.valueOf(scanner.next());
+
+        System.out.println("Ingrese el id de la editorial del libro: ");
+        int idEditorial = scanner.nextInt();
+
+        librodao.crearLibro(titulo, isbn, fechaPublicacion, idEditorial);
+    }
+
+    public void eliminarLibro(){
+        listarLibros();
+        System.out.println("\n Ingrese el id del libro a eliminar: ");
+        int idLibro = scanner.nextInt();
+
+        librodao.eliminarLibro(idLibro);
+    }
+
+    public void editarLibro(){
+        listarLibros();
+        System.out.println("\n Ingrese el id del libro a modificar: ");
+        int idLibro = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Ingrese el nuevo nombre del libro: ");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Ingrese el nuevo isbn del libro: ");
+        int isbn = scanner.nextInt();
+
+        System.out.println("Ingrese la fecha de publicacion del libro: ");
+        Date fechaPublicacion = Date.valueOf(scanner.next());
+
+        System.out.println("Ingrese el id de la editorial del libro: ");
+        int idEditorial = scanner.nextInt();
+
+        librodao.editarLibro(idLibro, nombre, isbn, fechaPublicacion, idEditorial);
+    }
+    public void listarLibros() {
+        List<Libro> libros = librodao.listarLibros();
+
+        if (libros.isEmpty()) {
+            System.out.println("No hay libros para mostrar");
+        } else {
+            for (Libro libro : libros) {
+                libro.mostrarInformacion();
+            }
+        }
+    }
+    public void crearEditorial() {
+        System.out.println("Ingrese el nombre de la editorial: ");
+        String nombre = scanner.nextLine();
+
+        editorialdao.crearEditorial(nombre);
+    }
+
+    public void eliminarEditorial(){
+        listarEditoriales();
+        System.out.println("Ingrese el id de la editorial a eliminar: ");
+        int idEditorial = scanner.nextInt();
+
+        editorialdao.eliminarEditorial(idEditorial);
+    }
+
+    public void editarEditorial(){
+        listarEditoriales();
+        System.out.println("Ingrese el id de la editorial a modificar: ");
+        int idEditorial = scanner.nextInt();
+
+        System.out.println("Ingrese el nuevo nombre de la editorial: ");
+        String nombre = scanner.nextLine();
+
+        editorialdao.editarEditorial(idEditorial, nombre);
+    }
+
+    public void listarEditoriales() {
+        List<Editorial> editoriales = editorialdao.listarEditorial();
+
+        if (editoriales.isEmpty()) {
+            System.out.println("No hay editoriales para mostrar");
+        } else {
+            for (Editorial editorial : editoriales) {
+                editorial.mostrarInformacion();
+            }
+        }
+    }
 
 
 }
