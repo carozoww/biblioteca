71900d6d984e4b42b9cd25bf062ff06e7820c641
Pilar <pilar.perez.s@estudiantes.utec.edu.uy>
2025-09-03 19:37:21 -0300

    resubir clase libro y dao

--- src/models/Libro.java
+++ src/models/Libro.java
@@ -1,4 +1,60 @@
 package models;
 
+import java.sql.Date;
+
 public class Libro {
+    private int idLibro;
+    private String titulo;
+    private int isbn;
+    private Date fechaPublicacion;
+    private int idEditorial;
+
+    public Libro(int idLibro, String titulo, int isbn, Date fechaPublicacion,  int idEditorial) {
+        this.idLibro = idLibro;
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.fechaPublicacion = fechaPublicacion;
+        this.idEditorial = idEditorial;
+    }
+    public int getIdLibro() {
+        return idLibro;
+    }
+    public void setIdLibro(int idLibro) {
+        this.idLibro = idLibro;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+    public Date getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(Date fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public int getIdEditorial() {
+        return idEditorial;
+    }
+
+    public void setIdEditorial(int idEditorial) {
+        this.idEditorial = idEditorial;
+    }
+    public void mostrarInformacion(){
+        System.out.println("Id del libro: " + idLibro);
+        System.out.println("Titulo: " + titulo);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Fecha de publicacion: " + fechaPublicacion);
+        System.out.println("Id de la editorial: " + idEditorial);
+
+    }
 }
