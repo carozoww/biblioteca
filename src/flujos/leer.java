a727e09290e22fed9207cdb8c78ab00e19c97cea
carlos <centurioncarlos991@gmail.com>
2025-09-18 19:19:54 -0300

    cambios a leer palabra

--- src/flujos/leer.java
+++ src/flujos/leer.java
@@ -6,6 +6,7 @@ public class leer {
 
 
     public String leerPalabra(Scanner sc){
+        sc.nextLine();
         String palabra =  sc.nextLine();
         while(palabra.isEmpty() || palabra == " "){
             if(palabra.isEmpty()){
