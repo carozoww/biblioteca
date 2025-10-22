3915749ab3af023421578e301efeabbfb0eece7b
carlos <centurioncarlos991@gmail.com>
2025-10-22 19:32:08 -0300

    funciones de listado de libro

---
+++ src/dao/SugerenciaDAO.java
@@ -0,0 +1,23 @@
+package dao;
+
+import basedatos.conexion;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SugerenciaDAO {
+
+    public void crearSugerencia(String contenido, int id_lector){
+        String query = "INSERT INTO sugerencia VALUES(?,?)";
+        try{
+            PreparedStatement ps = conexion.getInstancia().getConnection().prepareStatement(query);
+            ps.setString(1, contenido);
+            ps.setInt(2, id_lector);
+            ps.executeUpdate();
+
+            System.out.println("Sugerencia creada con exito!!!");
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
