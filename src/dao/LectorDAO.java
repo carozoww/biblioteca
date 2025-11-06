b723848c3ca43d930f1de8dbdc432e02b2bd4f7c
Pilar <pilar.perez.s@estudiantes.utec.edu.uy>
2025-11-06 15:02:28 -0300

    Update LectorDAO.java

--- src/dao/LectorDAO.java
+++ src/dao/LectorDAO.java
@@ -212,21 +212,24 @@ public class LectorDAO {
             throw new RuntimeException("Error al autenticar lector: " + idLector + e.getMessage());
         }
 
-    public int obtenerUltimaId(){
-        int num =0;
+      }
+
+    public int obtenerUltimaId() {
+        int num = 0;
         String query = "SELECT MAX(ID) FROM lector";
-        try{
+        try {
             Statement st = conexion.getInstancia().getConnection().createStatement();
             ResultSet rs = st.executeQuery(query);
 
-            while(rs.next()){
+            while (rs.next()) {
                 num = rs.getInt(1);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return num;
     }
+
 }
 
 
